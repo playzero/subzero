f1a3e483a80f7966625419b856fa6f0d10081b0f
2075 <2075@zero.io>
2021-01-13 14:42:32 +0100

    chore: sync runtime version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -104,17 +104,18 @@ pub fn wasm_binary_unwrap() -> &'static [u8] {
 }
 
 /// Runtime version.
+// Per convention: if the runtime behavior changes, increment spec_version
+// and set impl_version to 0. If only runtime
+// implementation changes and behavior does not, then leave spec_version as
+// is and increment impl_version.
+// ——> semver
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node"),
-	impl_name: create_runtime_str!("substrate-node"),
-	authoring_version: 10,
-	// Per convention: if the runtime behavior changes, increment spec_version
-	// and set impl_version to 0. If only runtime
-	// implementation changes and behavior does not, then leave spec_version as
-	// is and increment impl_version.
-	spec_version: 261,
+	spec_name: create_runtime_str!("subzero"),
+	impl_name: create_runtime_str!("alphaville"),
+	spec_version: 6,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
+	authoring_version: 10,
 	transaction_version: 2,
 };
 
